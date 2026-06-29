43430d0b10d15ab3fe989ee9968e5de434f93671
PranavMahesh55 <mahesh26p@ncssm.edu>
2026-06-29 15:14:01 -0400

    Fix Vercel API function export

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,1 +1,8 @@
-export { default } from "../apps/api/src/app";
+import { app } from "../apps/api/src/app";
+
+const handler = (req: Parameters<typeof app>[0], res: Parameters<typeof app>[1]) => {
+  return app(req, res);
+};
+
+export default handler;
+module.exports = handler;
